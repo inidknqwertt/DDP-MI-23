d340e243393f3bb358f3cf04c8440037e73a8b41
inidknqwertt <nilamdayukesuma21@gmail.com>
2023-10-15 21:42:10 +0700

    Array

---
+++ 05 Array/array 01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nilai[5];
+
+    nilai[0]=32;
+    nilai[0]=32;
+    nilai[0]=32;
+    nilai[0]=32;
+    nilai[0]=32;
+
+     cout << "Nilai ke-1 " << nilai[0] << endl;
+     cout << "Nilai ke-2 " << nilai[1] << endl;
+     cout << "Nilai ke-3 " << nilai[2] << endl;
+     cout << "Nilai ke-4 " << nilai[3] << endl;
+     cout << "Nilai ke-5 " << nilai[4] << endl;
+    
+return 0;
+     
+}
